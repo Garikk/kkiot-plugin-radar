13630a7a83e2dc70a3e0b7d0568215cb33352bab
Igor Blinov <i.blinov@um-agency.com>
2019-08-02 18:42:38 +0300

    default radar configuration (begin)

--- src/main/java/kkdev/kksystem/plugin/radar/manager/configuration/kk_DefaultConfig.java
+++ src/main/java/kkdev/kksystem/plugin/radar/manager/configuration/kk_DefaultConfig.java
@@ -23,8 +23,36 @@ public abstract class kk_DefaultConfig {
     public static RadarConf MakeDefaultConfig() {
 
         RadarConf DefConf = new RadarConf();
-        //
+        
+        RadarCluster rearRadar = new RadarCluster();
+        RadarCluster frontRadar = new RadarCluster();
+        RadarCluster sideRadar = new RadarCluster();
+        
+        rearRadar.Name = "Rear park radar";
+        rearRadar.Sensors = new RadarSensor[4];
+        rearRadar.Sensors[0] = initDefSensor(0,2000, 15);
+        rearRadar.Sensors[1] = initDefSensor(1,2000, 15);
+        rearRadar.Sensors[2] = initDefSensor(2,2000, 15);
+        rearRadar.Sensors[3] = initDefSensor(3,2000, 15);
+
+        rearRadar.Name = "Front park radar";
+        frontRadar.Sensors = new RadarSensor[2];
+        frontRadar.Sensors[0] = new RadarSensor();
+        frontRadar.Sensors[1] = new RadarSensor();
+
+        rearRadar.Name = "blind spot radar";
+        sideRadar.Sensors = new RadarSensor[2];
+        sideRadar.Sensors[0] = new RadarSensor();
+        sideRadar.Sensors[1] = new RadarSensor();
+
+
         
        return DefConf;
     }
+    private static RadarSensor initDefSensor(Integer idx, Integer range, Integer sector)
+    {
+        RadarSensor ret = new RadarSensor();
+        return ret;
+    }
+    
 }
